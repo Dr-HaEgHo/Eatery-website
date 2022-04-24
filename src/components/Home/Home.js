afccedb6131236aa6cb0cc7871920a55aa7d9762
Teem1345 <awogbuyitimothy@gmail.com>
2022-04-24 12:09:08 +0100

    added the assete to the src folder

---
+++ src/components/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Hero from '../Hero/Hero'
+import './home.css'
+
+
+const Home = () => {
+  return <div className='home-body'>
+        <Hero/>
+       
+
+        {/* stats */}
+       
+   
+
+        {/* About */}
+        {/* <About /> */}
+        
+        {/* Products */}
+        {/* <Products /> */}
+        
+        {/* Contact */}
+        {/* <Contact /> */}
+        
+        {/* Footer */}
+        {/* <Footer/> */}
+
+        {/* Testimonials */}
+        {/* <Testimonials/> */}
+    </div>
+}
+
+export default Home
